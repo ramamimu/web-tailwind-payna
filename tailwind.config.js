a1af9fa944b5bfa463ada0cdeb64ce363da15a65
ramamimu <ahhmaad.blogger@gmail.com>
2022-05-04 05:37:36 +0700

    inisial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
